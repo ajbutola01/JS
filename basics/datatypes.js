9df3fb5b8c084676d80f78f7189299493bc9cb0e
ajbutola01 <ajbutola01@gmail.com>
2026-06-17 06:03:51 +0000

    commit

---
+++ basics/datatypes.js
@@ -0,0 +1,5 @@
+"use strict";
+
+//alert("hello") we are using node JS not browser
+
+ 
---
+++ basics/variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail = "aj123@gmail.com"
+var accountPassword = 12345
+accountCity = "Hyderabad"
+let accountState;
+
+//accountId = 2
+
+accountEmail = "hchc@hc.com"
+accountPassword = "212121"
+accountCity = "Benguluru"
+
+console.log(accountId);
+console.table([accountEmail,accountId,accountCity,accountState,accountPassword])
