ca6666f9a0e9459a1abfa82ce12f22b2c1460386
ajbutola01 <ajbutola01@gmail.com>
2026-06-17 06:26:58 +0000

    Conversion of datatypes

---
+++ basics/conversionOperation.js
@@ -0,0 +1,11 @@
+let score = null
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+//let valueInNumber = Number(score)
+//console.log(valueInNumber);
+
+let IsLoggedIn = null;
+let booleanIsLoggedIn = Boolean(IsLoggedIn)
+console.log(booleanIsLoggedIn);
